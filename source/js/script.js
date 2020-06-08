e46be40b921309454b658c658fd3b4a4ef929ba6
dmitry ermolenko <dmitrijermolenko93@gmail.com>
2020-06-08 19:13:50 +0300

    рефакторинг шапки планшетной версии в открытом и закрытом состоянии

--- source/js/script.js
+++ source/js/script.js
@@ -1,19 +1,31 @@
-const header = document.querySelector('.header');
-const openButton = document.querySelector('.header__nav-toggle--burger');
-const closeButton = document.querySelector('.header__nav-toggle--cross');
+const openButton = document.querySelector('.header__nav-toggle-burger');
+const closeButton = document.querySelector('.header__nav-toggle-cross');
+const searchButton = document.querySelector('.header__search-button');
+const serviceList = document.querySelector('.header__service-list');
+const contactList = document.querySelector('.header__contact-list');
+const navButtonsWrapper = document.querySelector('.header__nav-buttons-wrapper');
+const navButtonsCentric = document.querySelector('.header__nav-buttons-centric');
 const navigation = document.querySelector('.header__navigation');
 const activeLinks = navigation.querySelectorAll('.site-list__link')
 
 openButton.addEventListener('click', () => {
-    header.classList.remove('header--nav-closed');
-    header.classList.add('header--nav-open');
-    navigation.classList.add('header__navigation--open')
+  openButton.classList.add('header__nav-toggle-burger--open');
+  closeButton.classList.add('header__nav-toggle-cross--open');
+  searchButton.classList.add('header__search-button--open');
+  navButtonsWrapper.classList.add('header__nav-buttons-wrapper--open');
+  navButtonsCentric.classList.add('header__nav-buttons-centric--open');
+  serviceList.classList.add('header__service-list--open');
+  contactList.classList.add('header__contact-list--open');
 });
 
 closeButton.addEventListener('click', () => {
-  header.classList.remove('header--nav-open');
-  header.classList.add('header--nav-closed');
-  navigation.classList.remove('header__navigation--open')
+  openButton.classList.remove('header__nav-toggle-burger--open');
+  closeButton.classList.remove('header__nav-toggle-cross--open');
+  searchButton.classList.remove('header__search-button--open');
+  serviceList.classList.remove('header__service-list--open');
+  contactList.classList.remove('header__contact-list--open');
+  navButtonsWrapper.classList.remove('header__nav-buttons-wrapper--open');
+  navButtonsCentric.classList.remove('header__nav-buttons-centric--open');
 });
 
 
