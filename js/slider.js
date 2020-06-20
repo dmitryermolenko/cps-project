05f2593a12e5cfb2fd901653c2f1d375b99c402a
dmitry ermolenko <dmitrijermolenko93@gmail.com>
2020-06-20 14:51:09 +0300

    Update 2020-06-20T11:51:00.467Z

---
+++ js/slider.js
@@ -0,0 +1,45 @@
+const sliderBrand = document.querySelector('.swiper-container--brand');
+const sliderType = document.querySelector('.swiper-container--type');
+
+
+let swiperBrand = new Swiper(sliderBrand, {
+	slidesPerView: 1,
+	spaceBetween: 15,
+	loop: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+  },
+
+  breakpoints: {
+    460: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+
+    660: {
+      slidesPerView: 3
+    }
+  }
+});
+
+let swiperType = new Swiper(sliderType, {
+  slidesPerView: 1,
+	spaceBetween: 15,
+	loop: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+  },
+
+  breakpoints: {
+    460: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+
+    660: {
+      slidesPerView: 3
+    }
+  }
+});
