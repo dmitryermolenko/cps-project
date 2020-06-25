5bdf3afdc9dc059cfd191ae1891ab3fd91e4bfd3
dmitry ermolenko <dmitrijermolenko93@gmail.com>
2020-06-26 00:22:31 +0300

    Updates

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,6 @@
 const sliderBrand = document.querySelector('.swiper-container--brand');
 const sliderType = document.querySelector('.swiper-container--type');
+const sliderPrices = document.querySelector('.swiper-container--prices');
 
 
 let swiperBrand = new Swiper(sliderBrand, {
@@ -41,5 +42,22 @@ let swiperType = new Swiper(sliderType, {
     660: {
       slidesPerView: 3
     }
+  }
+});
+
+let swiperPrices = new Swiper(sliderPrices, {
+  slidesPerView: 1,
+	spaceBetween: 15,
+	loop: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+  },
+
+  breakpoints: {
+    460: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
   }
 });
